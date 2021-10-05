672b4f4e118b9dca6707ff1d3cf4e81dbf463643
DmytroZeleniukh93 <d.zeleniukh1@gmail.com>
2021-10-05 21:24:16 +0300

    create class User

--- class_9.py
+++ class_9.py
@@ -21,3 +21,23 @@ restaurant1.open_restaurant()
 restaurant2 = Restaurant('Lol', 'Candy')
 restaurant2.describe_restaurant()
 restaurant2.open_restaurant()
+
+# 9-3 create class User
+
+
+class User:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def describe_user(self):
+        print(f'{self.first_name} {self.last_name} {self.age}')
+
+    def greet_user(self):
+        print(f'Hello {self.first_name}!')
+
+
+user1 = User('Alir', 'Kelo', '20')
+user1.describe_user()
+user1.greet_user()
